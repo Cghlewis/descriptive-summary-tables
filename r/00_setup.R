7427de37bf24a303b9814a59164bd5f38ab41ce0
Cghlewis <33465957+Cghlewis@users.noreply.github.com>
2021-10-01 15:47:40 -0500

    Update 00_setup.R
    Fix case sensitivity

--- r/00_setup.R
+++ r/00_setup.R
@@ -1,11 +1,11 @@
 
 # Descriptives for project documentation
 
-# Step 1: Run the script for each time period in order (ex: 01_C1Y1T1.R, 02_C1Y1T2.R)
+# Step 1: Run the script for each time period in order (ex: 01_c1y1t1.R, 02_c1y1t2.R)
     # a) If you are on a new time period that has not been created, then 
-      # 1. Open 01_C1Y1T1.R and change all instances of "C1Y1T1" to be the correct Cohort, Year, and Time Period
+      # 1. Open 01_c1y1t1.R and change all instances of "C1Y1T1" to be the correct Cohort, Year, and Time Period
       # 2. Change the data import at the top of the student and teacher section to the correct data for the time period
-      # 3. Save this syntax as 0#_C#Y#T#.R
+      # 3. Save this syntax as 0#_c#y#t#.R
       # 4. Then run this syntax as well as previous time periods
 
 # Step 2: Merge all time periods by measure into tables to be exported using the 99_combine-all-measures.R file. 
